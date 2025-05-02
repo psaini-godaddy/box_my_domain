4735c3eb9eecb31108eb6ad703f7ba6b3a319f12
psaini-godaddy <psaini@godaddy.com>
2025-05-02 14:01:17 +0530

    Update DomainListCard.js

--- src/DomainListCard.js
+++ src/DomainListCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Box, Button, Typography} from '@mui/material';
-import DomainChart from './DomainChart';
+import RadialBarChart from './DomainChart';
 
 const DomainListCard = ({ domains = [], confirmDomain, onRetry, price, result, fadeOut, chartData }) => {
     return (
@@ -14,10 +14,11 @@ const DomainListCard = ({ domains = [], confirmDomain, onRetry, price, result, f
               borderRight : fadeOut ? 'none': '3px solid black',
               borderTopRightRadius: fadeOut ? '0px': '12px',
               borderBottomRightRadius: fadeOut ? '0px' : '12px',
+              backgroundImage: !chartData ? "url('./bg-card.png')" :'none',
           }}
       >
           {!chartData && (
-          <Box style={{display: 'flex', marginTop : fadeOut ? '80px': '0px'}}>
+          <Box style={{display: 'flex'}}>
               <Typography variant="h3" align="center">
                   Congratulations 🎉 <br/>
                   <Typography variant="h4" align="center">
@@ -31,23 +32,34 @@ const DomainListCard = ({ domains = [], confirmDomain, onRetry, price, result, f
                   {domain}
               </Typography>
           ))}
+          {!chartData && result.trial_product && result.trial_product.trim() !== '' && (
+              <Typography
+                  variant="h6"
+                  style={{ fontSize: '30px', fontWeight: '600', marginTop: '-70px', padding: '20px' }}
+              >
+                  Free Trial : {result.trial_product
+                  .split('_')
+                  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                  .join(' ')}
+              </Typography>
+          )}
           {chartData && (
               <div style={{ marginTop: '-40px', width: '100%' }}>
-                  <DomainChart data={chartData} />
+                  <RadialBarChart data={chartData} />
               </div>
           )}
           {!fadeOut && (
               <Box style={{display: 'column'}}>
                   <Button
                       variant="contained"
-                      sx={{ backgroundColor: 'teal', textTransform: 'none', px: 3 , fontSize:'30px', marginRight: '100px'}}
+                      sx={{ backgroundColor: 'teal', textTransform: 'none', px: 3 , fontSize:'30px', marginRight: '100px', marginTop: '50px'}}
                       onClick={confirmDomain}
                   >
                       Love It!
                   </Button>
                   <Button
                       variant="contained"
-                      sx={{ backgroundColor: 'teal', textTransform: 'none', px: 3 , fontSize:'30px'}}
+                      sx={{ backgroundColor: 'teal', textTransform: 'none', px: 3 , fontSize:'30px', marginTop: '50px'}}
                       onClick={() => onRetry(price,'',result.session_id)}
                       disabled={result.remaining_rolls <= 0}
                   >Play Again! ({result.remaining_rolls} Retries Left)
